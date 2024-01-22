40dc6a3fec1f32143e497c30d9a9da29e6da046e
Ahmed-M-Aboutaleb <iifire005@gmail.com>
2024-01-22 05:05:39 +0200

    1475A

---
+++ Codeforces/900/1475A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define endl '\n'
+#define vi vector<int>
+#define vll vector<ll>
+using namespace std;
+void solve() {
+    ll n; cin >> n;
+    
+    if (log2(n) != floorl(log2(n))) {
+    cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
